e74a85e2a1736c11f517c1394cb0a3a3c3cba9a5
Narek-T <narek.tarverdyan@bk.ru>
2017-01-21 16:24:37 +0300

    init

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,1 +1,21 @@
-console.log('hello world');
+$(document).ready(function() {
+	$('.home-blog__list').slick({
+		dots: true,
+		infinite: true,
+		arrows: false,
+	});
+
+	$('.testimonials__list').slick({
+		dots: false,
+		infinite: true,
+		adaptiveHeight: true,
+		nextArrow: $('.testimonials-next'),
+		prevArrow: $('.testimonials-prev'),
+	});
+
+	$('.partners__list').slick({
+		dots: true,
+		infinite: true,
+		arrows: false,
+	});
+});
