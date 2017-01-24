906f4be23aed58e879f6baa12fffc8612d3257e1
Narek <narek.tarverdyan@bk.ru>
2017-01-24 15:05:32 +0300

    frontend done

--- dev/js/main.js
+++ dev/js/main.js
@@ -18,4 +18,27 @@ $(document).ready(function() {
 		infinite: true,
 		arrows: false,
 	});
+
+	$('.sidebar__menu > .menu-item-has-children > a ').click(function(event) {
+		event.preventDefault();
+		$(this).parent('li').toggleClass('active');
+	});
+
+	$('.sub-menu > .menu-item-has-children').prepend('<div class="submenu-arr-wrap"><span class="submenu-arr"></span></div>');
+
+	$('.submenu-arr-wrap').on('click', function(event) {
+		$(this).parent('li').toggleClass('active');
+	});
+
+	$('.order-service').magnificPopup({
+		type: 'inline',
+		fixedContentPos: true,
+		fixedBgPos: true,
+		overflowY: 'auto',
+		closeBtnInside: true,
+		preloader: false,
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-slide-bottom'
+	});
 });
